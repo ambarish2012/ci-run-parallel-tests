ef8988ac08f2a041211a74989fc85da030abce35
ambarish2012 <ambarish@shippable.com>
2018-02-19 14:59:08 -0800

    update sample

--- checkrunCIstatus.sh
+++ checkrunCIstatus.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 statusCode=20
 while [ $statusCode != 20 ]
 do
-  statusCode=$(curl -H "Authorization: apiToken 05267c17-ce42-464d-8770-cb34e05b8ee2" -H "Content-Type: application/json" https://api.shippable.com/runs/5a8b4b46ea36a107003cd194 | jq '.statusCode')
+  statusCode=$(curl -H "Authorization: apiToken 05267c17-ce42-464d-8770-cb34e05b8ee2" -H "Content-Type: application/json" https://api.shippable.com/runs/$runId | jq '.statusCode')
   echo $statusCode
 done
